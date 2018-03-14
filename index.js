359deb324fb1eea38409426e603983196c1c3cde
MaxMEllon <maxmellon1994@gmail.com>
2018-03-15 02:00:56 +0900

    Modules false for tree shacking

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ function buildPreset(context, opts) {
         '@babel/env',
         {
           'targets': {
+            'modules': false,
             'browsers': [
               'last 2 versions',
               'safari >= 7'
